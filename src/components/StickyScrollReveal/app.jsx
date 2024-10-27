2be715239766d2b5344911af06be6cb4de94f46c
AnkanaPanja <ankanapanja308@gmail.com>
2024-10-28 00:34:15 +0530

    modify data

--- src/components/StickyScrollReveal/app.jsx
+++ src/components/StickyScrollReveal/app.jsx
@@ -5,20 +5,20 @@ import Image from "../../assets/xx.png";
 
 const content = [
   {
-    title: "Collaborative Editing",
+    title: "Sweet Selections",
     description:
-      "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
+      "Experience effortless shopping in our Cart section! View, modify, and manage your bakery favorites with ease, and enjoy a secure, quick checkout that satisfies your cravings with just a tap.",
     content: (
       <div
         className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-        Collaborative Editing
+        Sweet Selections
       </div>
     ),
   },
   {
-    title: "Real time changes",
+    title: "Your Bakery Identity",
     description:
-      "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+      "Your personal hub for managing account details, viewing transactions, and updating preferences. Experience a seamless and secure journey tailored just for you, making your convenience our top priority at every step!",
     content: (
       <div className="h-full w-full  flex items-center justify-center text-white">
         <img
@@ -31,9 +31,9 @@ const content = [
     ),
   },
   {
-    title: "Version control",
+    title: "Source Access",
     description:
-      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+      "We prioritize your security with our OTP verification system. This robust authentication process ensures that only authorized users access their accounts, safeguarding your personal information while you enjoy exploring our delightful baked goods.",
     content: (
       <div
         className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
@@ -42,9 +42,9 @@ const content = [
     ),
   },
   {
-    title: "Running out of content",
+    title:"Baker's Buzz",
     description:
-      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+      "Stay in the loop with our bakery app's notification section! Get exclusive offers, discover new arrivals, and receive real-time updates on your orders for a seamless, personalized experience. Join us and savor every moment!",
     content: (
       <div
         className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
