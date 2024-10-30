8ce3b2f51947785e62a02918c4a098ef0ed49340
anuragjh <anuragjhav51@gmail.com>
2024-10-30 13:43:47 +0530

    Update app.jsx

--- src/components/TypewriterEffect/app.jsx
+++ src/components/TypewriterEffect/app.jsx
@@ -22,7 +22,7 @@ export function TypewriterEffectSmoothDemo() {
       </p>
       <TypewriterEffectSmooth words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <a href="Mthaiwala.apk" download="Mithaiwala.apk">
+        <a href="app-debug.apk" download="Mithaiwala.apk">
           <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
             Download
           </button>
